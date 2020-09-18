460526d082e08c640ec143aceae8a9fb0e8d7d49
Darius Nagy <nagydarius97@gmail.com>
2020-09-18 17:35:37 +0300

    WL-56 Added base data IT

--- src/intTest/kotlin/ro/cadeca/weasylearn/BaseDataIT.kt
+++ src/intTest/kotlin/ro/cadeca/weasylearn/BaseDataIT.kt
@@ -10,23 +10,32 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 import org.testcontainers.containers.MongoDBContainer
-
+import org.testcontainers.containers.PostgreSQLContainer
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(classes = [WeasylearnBeApplication::class], initializers = [MongoIT.Initializer::class])
+@ContextConfiguration(classes = [WeasylearnBeApplication::class], initializers = [BaseDataIT.Initializer::class])
 @ActiveProfiles(profiles = ["intTest"])
 @AutoConfigureMockMvc
-abstract class MongoIT {
+abstract class BaseDataIT {
     companion object {
         val mongo = MongoDBContainer()
+        val postgres = PostgreSQLContainer<Nothing>().apply {
+            withDatabaseName("postgres")
+            withUsername("integrationUser")
+            withPassword("testPass")
+        }
     }
 
     internal class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
         override fun initialize(configurableApplicationContext: ConfigurableApplicationContext) {
             mongo.start()
+            postgres.start()
 
             TestPropertyValues.of(
+                    "spring.datasource.url=${postgres.jdbcUrl}",
+                    "spring.datasource.username=${postgres.username}",
+                    "spring.datasource.password=${postgres.password}",
                     "spring.data.mongodb.port=${mongo.firstMappedPort}",
                     "spring.data.mongodb.host=${mongo.host}"
             ).applyTo(configurableApplicationContext.environment)
